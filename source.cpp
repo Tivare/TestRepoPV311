08ceac65b51b442693381ccd3a35fd4f0d77c868
Khylko Andrii <hilko_a@itstep.academy>
2024-04-18 20:22:26 +0300

    Initial commit

---
+++ source.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include "sum.h"
+#include "prod.h"
+#include <cmath>
+#include "dif.h"
+#include "quot.h"
+
+using namespace std;
+
+int main()
+{
+	cout<<"Hello world!"<<endl;
+	cout<<"Sum = "<<sum(12,23)<<endl;
+	cout<<"Prod = "<<prod(12,23)<<endl;
+	cout<<"Dif = "<<dif(12,23)<<endl;
+	cout<<"Pow = "<<pow(12,2)<<endl;
+	cout<<"Quot = "<<quot(12,23)<<endl;
+}
